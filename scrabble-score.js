0695ed2350f2cdc35e4e17d4703e264206e4b4ca
rw4n99 <rowangodfrey2@gmail.com>
2024-08-06 14:10:48 +0100

    Added letters C & D

--- scrabble-score.js
+++ scrabble-score.js
@@ -1,11 +1,16 @@
 export function calculateScrabbleScore(word) {
-    const scores = {
-        A: 1, B: 3
-    };
-    if (word === 'B');
-        return (3)
-    
-    //if (word =='A') return 1
+    //  const scores = {
+    //      A: 1, B: 3
+    // };
+    if (word === 'A'){
+        return 1      
+    }    
+    if (word ==='B' || 'C'){
+        return 3
+    }
+    if (word === 'D'){
+        return 2
+    }
 }
 
 
--- scrabble-score.test.js
+++ scrabble-score.test.js
@@ -6,11 +6,24 @@ test('mickeys-fishey-test', () => {
 })
 
 // test that checks whether `calculateScrabbleScore` correctly calculates the score for the word `A`
-test('mickeys-fishey-test', () => {
+test('mickeys-fishey-test-a', () => {
     expect(calculateScrabbleScore('A')).toBe(1)
 })
 
 // calculateScrabbleScore for word `B`
-test('mickeys-fishey-test', () => {
+test('mickeys-fishey-test-b', () => {
     expect(calculateScrabbleScore('B')).toBe(3)
 })
+
+//calculateScrabbleScore for word `C`
+test('mickeys-fishey-test-c', () => {
+    expect(calculateScrabbleScore('C')).toBe(3)
+});
+
+// test.each('mickeys-fishey-test-c', () => {
+//     expect(calculateScrabbleScore)
+// })
+
+test('mickeys-fishey-test-d', () => {
+    expect(calculateScrabbleScore('D')).toBe(2)
+});
